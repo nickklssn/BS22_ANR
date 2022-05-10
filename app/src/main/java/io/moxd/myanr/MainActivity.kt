d7f129acc7eea0b1daf07b6ff58dc0cc60917907
Nick Klassen <nick.klassen@smail.th-koeln.de>
2022-05-10 22:35:15 +0200

    added handler in run method

--- app/src/main/java/io/moxd/myanr/MainActivity.kt
+++ app/src/main/java/io/moxd/myanr/MainActivity.kt
@@ -2,6 +2,8 @@ package io.moxd.myanr
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,7 +21,6 @@ class MainActivity : AppCompatActivity() {
         textView.text = "[ placeholder for some random values ]"
 
 
-
         findViewById<Button>(R.id.run_button).setOnClickListener {
             run()
 
@@ -28,29 +29,55 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.stop_button).setOnClickListener {
             stop()
         }
+
     }
+
 
     // Erwartung: Der Run-Button liefert durchgehend randomisierte Werte über die TextView
     // Realität: Ein Klick auf den Run-Button lässt die App nicht mehr reagieren, keine Werte werden angezeigt und die App stürzt ab
 
-    private fun run() {
+
+    // Alte Run-Methode
+/*    private fun run() {
 
         while (true) {
             textView.text = Random.nextInt().toString()
+
         }
+    }*/
+
+
+     private fun run() {
+
+        var an = true
+
+        val mainHandler = Handler(Looper.getMainLooper())
+
+        mainHandler.post(object : Runnable {
+            override fun run() {
+
+                if (an) {
+                    mainHandler.postDelayed(this, 1500)
+                    textView.text = Random.nextInt().toString()
+                }
+            }
+        })
     }
 
-    private fun stop() {
 
-        textView.text = "[ placeholder for some random values ]"
+     private fun stop() {
+
     }
 
-    // Der Android-Profiler ist ein Echtzeit-Tool, das die benötigten Ressourcen CPU, Speicher, Netzwerk und Akku eigener Apps anzeigt
-    // Je nachdem, welche Aktion durchgeführt wird, werden unterschiedliche Ressourcen verwendet
-    // Beim einfachen Tippen auf das Display wird die CPU in einem sehr geringen Ausmaß genutzt (Speicher wird nicht gebraucht)
-    // Beim Tippen auf den Start-Button hingegen werden alle Ressourcen verwendet (CPU, Speicher und Akku werden belastet)
 
-    //Erklärung: Es werden scheinbar viele randomisierte Zahlen ausgegeben (hohe CPU-Auslastung + Memory-Nutzung) -> es kommt zum Stack-Overflow -> App stürzt ab
-    // Hohe Energienutzung durch erhöhte CPU-Auslastung und Memory-Nutzung
 
-}
+
+        // Der Android-Profiler ist ein Echtzeit-Tool, das die benötigten Ressourcen CPU, Speicher, Netzwerk und Akku eigener Apps anzeigt
+        // Je nachdem, welche Aktion durchgeführt wird, werden unterschiedliche Ressourcen verwendet
+        // Beim einfachen Tippen auf das Display wird die CPU in einem sehr geringen Ausmaß genutzt (Speicher wird nicht gebraucht)
+        // Beim Tippen auf den Start-Button hingegen werden alle Ressourcen verwendet (CPU, Speicher und Akku werden belastet)
+
+        //Erklärung: Es werden scheinbar viele randomisierte Zahlen ausgegeben (hohe CPU-Auslastung + Memory-Nutzung) -> es kommt zum Stack-Overflow -> App stürzt ab
+        // Hohe Energienutzung durch erhöhte CPU-Auslastung und Memory-Nutzung
+
+    }
